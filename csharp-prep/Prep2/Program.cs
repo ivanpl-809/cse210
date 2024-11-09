97c3fa6f81e5faf1649cd1a7580b5145609f22f2
ivan-pl809 <pon20007@byui.edu>
2024-11-08 20:40:56 -0600

    com

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+         {
+         Console.Write("Enter your grade percentage: ");
+            int percentage = int.Parse(Console.ReadLine());
+
+            string letter;
+            string sign = "";
+
+            if (percentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (percentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (percentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (percentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            if (letter != "A" && letter != "F")
+            {
+                int lastDigit = percentage % 10;
+
+                if (lastDigit >= 7)
+                {
+                    sign = "+";
+                }
+                else if (lastDigit < 3)
+                {
+                    sign = "-";
+                }
+            }
+
+            else if (letter == "A" && percentage < 93)
+            {
+                sign = "-";
+            }
+
+
+            Console.WriteLine($"Your grade is: {letter}{sign}");
+
+            if (percentage >= 70)
+            {
+                Console.WriteLine("Congratulations, you passed the course!");
+            }
+            else
+            {
+                Console.WriteLine("Keep trying, you'll get it next time!");
+            }
     }
+}
 }
